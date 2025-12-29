a007f68e48b82b21208ea2db257768628904613c
Uliana Kondratiuk <ulianakondratiuk@pm.me>
2025-12-29 19:27:11 +0000

    Move back to CET

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,7 +92,7 @@ in
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  time.timeZone = "Europe/London";
+  time.timeZone = "Europe/Zurich";
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
